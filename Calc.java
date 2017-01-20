4f84db39513a4b52b78ae8e88937842f45ba645c
jessicaannor <annor.j@gmail.com>
2017-01-20 17:45:07 +0000

    I have committed

---
+++ Calc.java
@@ -0,0 +1,12 @@
+public class Calc;
+{
+	public static void main (String [] args){
+		if(args[0].equals("add")){
+			num1 = Double.parseDouble(args[1]);
+			num2 = Double.parseDouble(args[2]);
+
+			System.out.println(num1 + num2);
+
+		}
+	}
+}
