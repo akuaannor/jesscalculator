7a12fa9ce68306c89305ce395da32f51b8c77e6c
jessicaannor <annor.j@gmail.com>
2017-01-23 13:27:26 +0000

    I have committed my Subtract function

--- Calc.java
+++ Calc.java
@@ -8,5 +8,12 @@ public class Calc;
 			System.out.println(num1 + num2);
 
 		}
+			
+		if(args[0].equals("subtract")){
+			num1 = Double.parseDouble(args[1]);
+			num2 = Double.parseDouble(args[2]);
+
+			System.out.println(num1 - num2);	
+
 	}
 }
