7f23f34a2e9fff787c22be5227f7fd729eed23c9
jessicaannor <annor.j@gmail.com>
2017-01-23 13:31:00 +0000

    I have committed my Multiplication function

--- Calc.java
+++ Calc.java
@@ -8,5 +8,18 @@ public class Calc;
 			System.out.println(num1 + num2);
 
 		}
+		else if(args[0].equals("Subtract")){
+			num1 = Double.parseDouble(args[1]);
+			num2 = Double.parseDouble(args[2]);
+
+			System.out.println(num1 - num2);
+		}
+			
+			else if(args[0].equals("Multiply")){
+			num1 = Double.parseDouble(args[1]);
+			num2 = Double.parseDouble(args[2]);
+
+			System.out.println(num1 * num2);
 	}
+}
 }
