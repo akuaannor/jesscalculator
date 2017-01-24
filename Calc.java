55da346e0bbcf6cec94fd43fc10c991830671515
adwoaarmah <adwoa.armah@ashesi.edu.gh>
2017-01-24 00:41:29 +0000

    Divide committed

--- Calc.java
+++ Calc.java
@@ -1,6 +1,7 @@
 public class Calc;
 {
 	public static void main (String [] args){
+		double num1, num2;
 		if(args[0].equals("add")){
 			num1 = Double.parseDouble(args[1]);
 			num2 = Double.parseDouble(args[2]);
@@ -21,6 +22,14 @@ public class Calc;
 			num2 = Double.parseDouble(args[2]);
 
 			System.out.println(num1 * num2);
+
+	}
+
+	if (args[0].equals(divide)){
+		num1 = Double.parseDouble(args[1]);
+		num2 = Double.parseDouble(args[2]);
+
+		System.out.println(num1 / num2);
 
 	}
 }
